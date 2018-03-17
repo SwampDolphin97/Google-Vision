aa7f333fd806f4c5ee42a18d0f03bce1b513f5ec
XHunter@Kartikey <Kushwah.kartikey14@stu.upes.ac.in>
2018-03-18 01:46:30 +0530

    Code and Example Copy Added

---
+++ vision.py
@@ -0,0 +1,24 @@
+import os
+import io
+from google.cloud.vision import types
+from google.cloud import vision
+import pprint
+
+def detect_soil(path):
+    """Detects labels in the file."""
+    client = vision.ImageAnnotatorClient()
+
+    with io.open(path, 'rb') as image_file:
+        content = image_file.read()
+
+    image = types.Image(content=content)
+
+    response = client.label_detection(image=image)
+    labels = response.label_annotations
+    print('What I have found')
+	
+    flag=0	
+    for label in labels:
+	print(label.description)
+filename = raw_input("Enter filename:")
+detect_soil(filename)
